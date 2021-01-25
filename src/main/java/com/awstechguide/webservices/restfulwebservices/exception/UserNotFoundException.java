f38f40a89db657877972b3c0b6eda30d0307feb7
sankalp <sankalpmishra@gmail.com>
2021-01-25 13:29:04 +0530

    first commit

--- src/main/java/com/awstechguide/webservices/restfulwebservices/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.awstechguide.webservices.restfulwebservices.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-}
